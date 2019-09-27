b8aafe021ddfdcf0c0c392ab1531b0264fa1c32a
DavidCasas19 <54973640+DavidCasas19@users.noreply.github.com>
2019-09-27 16:32:40 -0700

    Add files via upload

---
+++ T04.-Metodo Out/T04.-Metodo Out/Datos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T04._Metodo_Out
+{
+    class Datos
+    {
+        //Hacemos un metodo donde convertimos los centavos a pesos
+        //en el metodo le pasamos los parametros de progam con out
+       public void Convertir(double Totalcentavos, out double Pesos, out double Centavos)
+        {
+            Pesos = Totalcentavos / 100;
+
+            Centavos = Totalcentavos;
+        }
+        
+    }
+
+}
---
+++ T04.-Metodo Out/T04.-Metodo Out/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T04._Metodo_Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaramos las variables que vamos a utilizar
+            double TotalCentavo;
+            double Pesos = 0;
+            double Centavos = 0;
+           
+            //le damos la bienvenida al usuario y le pedimos que introduzca el valor en centavos
+            Console.WriteLine("Hola bienvenido");
+            Console.WriteLine("    ");
+            Console.WriteLine("Introduzca la cantidad en Centavos:");
+            TotalCentavo = Convert.ToDouble(Console.ReadLine());
+            Console.ReadKey();
+            Console.WriteLine("   ");
+            //instanciamos la clase datos para mandar a llamar al metodo convertir 
+            Datos da = new Datos();
+            //Mandamos los parametros para el metodo Convertir
+            da.Convertir(TotalCentavo, out Pesos, out Centavos);
+
+            //le mostramos al usuario el resultado en centavos y en pesos
+            Console.WriteLine("Total en centavos:");
+            Console.WriteLine(Centavos);
+            Console.WriteLine("   ");
+            Console.ReadLine();
+            Console.WriteLine("Total en pesos:");
+            Console.WriteLine(Pesos);
+            Console.ReadKey();
+        }
+
+
+    }
+}
